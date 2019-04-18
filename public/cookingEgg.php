1ed537627917a040c7c89ff9eff9ed5b0dbcb9f9
AdelineDb <adeline.dubosc@hotmail.fr>
2019-04-18 22:36:37 +0200

    ajout css

--- public/cookingEgg.php
+++ public/cookingEgg.php
@@ -11,16 +11,23 @@ $eggs = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/eg
 
 <h2>Maintenant crée ton oeuf parfait pour la reine</h2>
 
-<div class="allTheCards">
+<div class="allTheCards1">
     <?php foreach ($eggs as $egg => $value) { ?>
-        <div class="block_cards"> <?php
-            if ($eggs[$egg]['id'] == "5cac51240d488f0da6151bf5" || $eggs[$egg]['id'] == "5cac51240d488f0da6151c07" || $eggs[$egg]['id'] == "5cac51240d488f0da6151be0" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bf3" || $eggs[$egg]['id'] == "5cac51240d488f0da6151c09" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bcd" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bd1" || $eggs[$egg]['id'] == "5cac51240d488f0da6151be6" || $eggs[$egg]['id'] == "5cac51240d488f0da6151c14" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bde" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bd3" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bdf" || $eggs[$egg]['id'] == "5cac51240d488f0da6151be3" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bed") {
-                if ($eggs[$egg]['id'] == "5cac51240d488f0da6151c07" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bed" || $eggs[$egg]['id'] == "5cac51240d488f0da6151c14" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bf3") {
+        <div class="block_cards1"> <?php
+            if ($eggs[$egg]['id'] == "5cac51240d488f0da6151bf5" || $eggs[$egg]['id'] == "5cac51240d488f0da6151c07" ||
+                $eggs[$egg]['id'] == "5cac51240d488f0da6151be0" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bf3" ||
+                $eggs[$egg]['id'] == "5cac51240d488f0da6151c09" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bcd" ||
+                $eggs[$egg]['id'] == "5cac51240d488f0da6151bd1" || $eggs[$egg]['id'] == "5cac51240d488f0da6151be6" ||
+                $eggs[$egg]['id'] == "5cac51240d488f0da6151c14" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bde" ||
+                $eggs[$egg]['id'] == "5cac51240d488f0da6151bd3" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bdf" ||
+                $eggs[$egg]['id'] == "5cac51240d488f0da6151be3" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bed") {
+                //if ($eggs[$egg]['id'] == "5cac51240d488f0da6151c07" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bed"
+                // || $eggs[$egg]['id'] == "5cac51240d488f0da6151c14" || $eggs[$egg]['id'] == "5cac51240d488f0da6151bf3") {
                     ?>
                     <div><img src="<?php echo $eggs[$egg]['image']; ?>" alt="oeuf" width="150px"></div>
-                <?php } else { ?>
+                <?php //} else { ?>
                     <div><img src="img/oeufmystèrev1.jpg" alt="oeuf" width="150px"></div> <?php
-                }
+                //}
                 ?>
                 <p class="titreCards"> <?php echo $eggs[$egg]['name']; ?> </p>
                 <p class="descriptionCards"> <?php echo 'Calibre ' . $eggs[$egg]['caliber']; ?> </p>
