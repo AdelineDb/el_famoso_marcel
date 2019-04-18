6696f66ccb208bdeca5f5731fadb9b5ae6706e4b
AdelineDb <adeline.dubosc@hotmail.fr>
2019-04-18 16:13:15 +0200

    tableau perso

--- public/index.php
+++ public/index.php
@@ -5,13 +5,58 @@ echo '<pre>';
 var_dump($egg);
 echo '</pre>';
 
-foreach ($egg as $key=>$value) {
-    $eggspower = 'les pouvoirs cet oeuf sont :' . $egg[$key]['name'] . '<br>';
+foreach ($egg as $key => $value) {
+    $eggspower = 'Oeuf :' . $egg[$key]['name'] . '<br>';
     echo $eggspower;
 }
+echo 'PERSONNAGES';
 
-
-$char = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/characters"));
+$char = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/characters"), true);
 echo '<pre>';
+?>
+<table>
+    <thead>
+    <tr>
+        <td>name</td>
+        <td>gender</td>
+        <td>origin</td>
+        <td>image</td>
+        <td>skills</td>
+    </tr>
+    </thead>
+    <tbody>
 
-echo '</pre>';
+    <?php
+    foreach ($char as $key => $value) {
+        echo '<tr> <td>';
+        $charName = $char[$key]['name'];
+        echo $charName;
+        echo '</td>';
+
+        echo '<td>';
+        $charGender = $char[$key]['gender'];
+        echo $charGender;
+        echo '</td> ';
+
+        echo '<td>';
+        $charOrigin = $char[$key]['origin'];
+        echo $charOrigin;
+        echo '</td>';
+
+        echo '<td>';
+        $charImg = $char[$key]['image'];?>
+        <img src="<?= $charImg;?>" alt="..." width="150px">
+        </td>
+
+        <td> <ul><?php
+        '<li>'; $charSkills = $char[$key]['skills'][0];
+        echo $charSkills; '<br></li>';
+        '<li>'; $charSkills1 = $char[$key]['skills'][1];
+        echo $charSkills1; '<br> </li>';
+        '<li>'; $charSkills2 = $char[$key]['skills'][2];
+        echo $charSkills2; '</li>';
+        echo '</ul></td> </tr>';
+    } ?>
+    </tbody>
+</table>
+
