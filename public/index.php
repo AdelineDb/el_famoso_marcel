edcecfb6a77e5a7b535f49dd690e3cd6931e49f3
Arnaud Auzou <arnaud.auzoudm@gmail.com>
2019-04-18 21:01:47 +0200

    Home V1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,37 @@
 <?php
 
 require_once('_head.php');
+
+?>
+<body class="home">
+
+<section>
+    <div class="central-home">
+        <div>
+            <img src="https://images.innoveduc.fr/easter_hackathon/Cersei_Lannister.png" alt="Cersei Lannister">
+            <div class="cersei-center">
+            <p>LA REINE</p>
+            <p>Cersei Lannister</p>
+            <p>Humaine</p>
+            </div>
+        </div>
+
+        <div class="home-intro-space">
+            <h2 class="home-intro">J'ai faim !</br>
+            Apporte-moi le meilleur oeuf de </br>
+            Pâques et je te récompenserai !</br>
+            Néanmoins attention, ton mélange</br>
+            pourrait avoir de graves</br>
+            conséquences sur le royaume...</h2>
+        </div>
+    </div>
+    <div class="btn-center">
+        <button class="btn-home" type="button">
+            JE JOUE
+        </button>
+    </div>
+
+</section>
+
+</body>
+</html>
