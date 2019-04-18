8dabe99233334541d5cfeff04ffe570a323b01ab
Pascal Encinas <krionari@gmail.com>
2019-04-18 18:18:04 +0200

    commencement page character + recupération header

---
+++ public/choixPersonnage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Titre</title>
+</head>
+
+<h2>Choisis ton personnage</h2>
+
+<?php
+$characters = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/characters"));
+
+
+?>
+
+<div class="block_cards">
+    <a href=""><img class="photo_miniature" src="" alt=""></a>
+
+    <a href=""><div class="">
+            <p class=""></p>
+            <p class=""></p>
+        </div></a>
+
+    <div class="">
+        <p class=""></p>
+    </div>
+</div>
+
+    <body>
+
+    </body>
+</html>
+
+
+
