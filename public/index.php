456fa1781a1dd14c15c101c740fc319eac34d1d0
Poupimali <poupimali@gmail.com>
2019-04-18 17:46:07 +0200

    images ok

--- public/index.php
+++ public/index.php
@@ -1,4 +1,22 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$egg = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/eggs"), true);
+/*echo '<pre>';
+var_dump($egg);
+echo '</pre>';*/
+
+foreach ($egg as $key=>$value) {
+    $eggspower = 'les pouvoirs cet oeuf sont :' . $egg[$key]['name'] . '<br>';
+    echo $eggspower;
+}
+
+$char = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/characters"));
+
+foreach ($char as $key=>$value) {
+    $name = 'les noms des charactères sont :' . $char[$key]['name'] . '<br>';
+    echo $name;
+}
+/*echo '<pre>';
+var_dump($char);
+echo '</pre>';*/
 
