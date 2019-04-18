d75735d8a1a9f31725aec5185ee664bf3414fad2
AdelineDb <adeline.dubosc@hotmail.fr>
2019-04-18 18:22:56 +0200

    ajout cookingEgg

---
+++ public/cookingEgg.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$eggs = json_decode(file_get_contents("http://easteregg.wildcodeschool.fr/api/eggs")); ?>
+
+
+<h2>Maintenant crée ton oeuf parfait pour la reine</h2>
+
+
+
+
+</body>
+</html>
