7224e89b46eea73d4739ae7f09d3493871cdac8f
Pascal Encinas <krionari@gmail.com>
2019-04-19 10:27:33 +0200

    bug fix de l'année

--- public/checkFormEgg.php
+++ public/checkFormEgg.php
@@ -17,35 +17,26 @@ if (!isset($_SESSION['egg'])) {
 <section class="cookies container-fluid">
     <div class="row">
 
-        <?php echo '<pre>';
-        var_dump($_SESSION['egg']);
-        var_dump($esterEgg);
-        echo '</pre>' ?>
-        <ul>
-            <?php
-            $succes = 0;
-            ksort($esterEgg);
-            ksort($_SESSION['egg']);
+        <?php
+        $succes = 0;
+        ksort($esterEgg);
+        ksort($_SESSION['egg']);
 
-
-            if (serialize($esterEgg) == serialize($_SESSION['egg'])) {
-                header('Location: fin3.php');
-            }
-
-            var_dump(serialize($esterEgg));
-            var_dump(serialize($_SESSION['egg']));
-            foreach ($_SESSION['egg'] as $id => $egg) {
+        foreach ($_SESSION['egg'] as $id => $egg) {
             if ($id == "5cac51240d488f0da6151c07" || $id == "5cac51240d488f0da6151bd1") {
                 $succes += 1;}
 
-            }
-            if ($succes == 2) {
+        }
+        if ($succes == 2) {
             header('Location: fin1.php');
-            } else {
+        } else {
             header('Location: fin2.php');
-            }
-            ?>
-        </ul>
+        }
+
+        if (serialize($esterEgg) == serialize($_SESSION['egg'])) {
+            header('Location: fin3.php');
+        }
+        ?>
     </div>
 </section>
 
